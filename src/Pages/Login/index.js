30ce8b74afeb2351a37f681528059c9f7b7713f7
noahZhu <noah.zhu@austintechno.com>
2021-09-26 15:59:10 +0800

    first commit

---
+++ src/Pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'antd'
+
+const Login = () => (
+  <div>
+    <p>auPay商户中心</p>
+    <Button>登录啦</Button>
+  </div>
+)
+
+export default Login
